f64d241e72740a1c4839a3ef1a7e9d5626b34189
alymkulovryspai1412@gmail.com <alymkulovryspai1412@gmail.com>
2022-12-23 13:57:04 +0600

    diplom

--- django_kepka14/urls.py
+++ django_kepka14/urls.py
@@ -20,13 +20,13 @@ urlpatterns = [
     path('api/v1/socialMedia/', include('social_media.urls')),
 
     path(r'api/v1/', include(router.urls)),
-    path('caps/<int:id>/', views.CapsUpdateDeleteAPIView.as_view()),
-    path('api/v1/maimmenu/', views.MainMenuListAPIView.as_view()),
-    path('categories/', views.CategoryListCreateAPIView.as_view()),
-    path('brands/<int:id>/', views.BrandUpdateDeleteAPIView.as_view()),
-    path('categories/<int:id>/', views.CategoryUpdateDeleteAPIView.as_view()),
-    path('manufacturer/', views.ManufacturerListAPIview.as_view()),
-    path('manufacturer/<int:id>/', views.ManufacturerItemUpdateDeleteAPIview.as_view()),
+    path('api/v1/caps/<int:id>/', views.CapsUpdateDeleteAPIView.as_view()),
+    path('api/v1/api/v1/maimmenu/', views.MainMenuListAPIView.as_view()),
+    path('api/v1/categories/', views.CategoryListCreateAPIView.as_view()),
+    path('api/v1/brands/<int:id>/', views.BrandUpdateDeleteAPIView.as_view()),
+    path('api/v1/categories/<int:id>/', views.CategoryUpdateDeleteAPIView.as_view()),
+    path('api/v1/manufacturer/', views.ManufacturerListAPIview.as_view()),
+    path('api/v1/manufacturer/<int:id>/', views.ManufacturerItemUpdateDeleteAPIview.as_view()),
 
     ] + cart_urls + swagger_urls + discount_urls
 
